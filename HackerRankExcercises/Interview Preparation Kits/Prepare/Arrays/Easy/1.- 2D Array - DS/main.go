0525b80dba8bcfa9a3d3bb967294bda472cce225
Sam Seda <zyxsamys@gmail.com>
2023-11-26 20:19:16 -0600

    Excercices - Easy Arrays - 2D Array

---
+++ HackerRankExcercises/Interview Preparation Kits/Prepare/Arrays/Easy/1.- 2D Array - DS/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+func hourglassSum(arr [][]int32) int32 {
+	// Write your code here
+	// MaxSum record
+	var maxSum int32 = -99999999
+	// For to iterate in y
+	for y := 0; y < 4; y++ {
+		// For to iterate in x
+		for x := 0; x < 4; x++ {
+			var tempSum int32
+			// Sum according hourglass form [00, 01, 02, 11, 20, 21, 22]
+			tempSum = arr[x][y] + arr[x][y+1] + arr[x][y+2] + arr[x+1][y+1] + arr[x+2][y] + arr[x+2][y+1] + arr[x+2][y+2]
+			fmt.Println(tempSum)
+
+			// Compare if sum is > MaxSum
+			if tempSum > maxSum {
+				maxSum = tempSum
+			}
+		}
+	}
+	return maxSum
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	var arr [][]int32
+	for i := 0; i < 6; i++ {
+		arrRowTemp := strings.Split(strings.TrimRight(readLine(reader), " \t\r\n"), " ")
+
+		var arrRow []int32
+		for _, arrRowItem := range arrRowTemp {
+			arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
+			checkError(err)
+			arrItem := int32(arrItemTemp)
+			arrRow = append(arrRow, arrItem)
+		}
+
+		if len(arrRow) != 6 {
+			panic("Bad input")
+		}
+
+		arr = append(arr, arrRow)
+	}
+
+	result := hourglassSum(arr)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
