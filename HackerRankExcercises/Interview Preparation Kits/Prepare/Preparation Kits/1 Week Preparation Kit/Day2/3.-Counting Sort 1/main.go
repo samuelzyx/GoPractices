2100b8b207f651a5fd7a48276b7cf38d34d1f186
Sam Seda <zyxsamys@gmail.com>
2023-11-27 19:42:59 -0600

    Interview Kit Preparation Counting Sort 1

---
+++ HackerRankExcercises/Interview Preparation Kits/Prepare/Preparation Kits/1 Week Preparation Kit/Day2/3.-Counting Sort 1/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func countingSort(arr []int32) []int32 {
+	// Write your code here
+	// Get arr len
+	lenArr := len(arr)
+	var res []int32
+	//Note: For this exercise, always return a frequency
+	//array with "100 elements"...
+	//Fill array of 100 positions with 0
+	for i := 0; i < 100; i++ {
+		res = append(res, 0)
+	}
+
+	for i := 0; i < lenArr; i++ {
+		//According the value of arr[i] it will increment the
+		//position in res[arr[i]] in one
+		//if arr[i] == 3 then res[3] = res[3]+1
+		//values/index 0 1 2 3 ... 100
+		//res          0 0 0 1 <----
+		res[arr[i]] = res[arr[i]] + 1
+	}
+	return res
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	result := countingSort(arr)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, " ")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
