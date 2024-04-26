e4449d485358e67f910b2bab908b96da2ad6cda8
Sam Seda <zyxsamys@gmail.com>
2024-04-25 21:55:29 -0600

    Codesignal CheckPalindrome

---
+++ CodeSignalExcercices/3.CheckPalindrome/solution.go
@@ -0,0 +1,11 @@
+func solution(inputString string) bool {
+	splitStr := strings.Split(inputString, "")
+
+	for i, j := 0, len(splitStr)-1; i < j; i, j = i+1, j-1 {
+		splitStr[i], splitStr[j] = splitStr[j], splitStr[i]
+	}
+
+	reverseStr := strings.Join(splitStr, "")
+
+	return reverseStr == inputString
+}
