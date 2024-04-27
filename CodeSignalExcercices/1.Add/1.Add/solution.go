cab2a9b51606eb6715c2a52d5e0b7c6c6f895fc0
Sam Seda <zyxsamys@gmail.com>
2024-04-26 18:52:48 -0600

    Adding comments to search on future references

--- CodeSignalExcercices/1.Add/1.Add/solution.go
+++ CodeSignalExcercices/1.Add/1.Add/solution.go
@@ -1,10 +1,10 @@
 func solution(n int) int {
-	strNum := strconv.Itoa(n)
+	strNum := strconv.Itoa(n) //Int to String
 	nArrStr := strings.Split(strNum, "")
 	result := 0
 
 	for i := 0; i < len(nArrStr); i++ {
-		num, _ := strconv.Atoi(nArrStr[i])
+		num, _ := strconv.Atoi(nArrStr[i]) //String to int
 		result += num
 	}
 
--- CodeSignalExcercices/3.CheckPalindrome/solution.go
+++ CodeSignalExcercices/3.CheckPalindrome/solution.go
@@ -1,7 +1,7 @@
 func solution(inputString string) bool {
 	splitStr := strings.Split(inputString, "")
 
-	for i, j := 0, len(splitStr)-1; i < j; i, j = i+1, j-1 {
+	for i, j := 0, len(splitStr)-1; i < j; i, j = i+1, j-1 { //Reverse array
 		splitStr[i], splitStr[j] = splitStr[j], splitStr[i]
 	}
 
