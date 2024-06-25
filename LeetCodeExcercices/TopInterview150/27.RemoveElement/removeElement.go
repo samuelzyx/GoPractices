e96e9331b8f218c916618d61524fc384f18e279e
Ing Samuel Seda from NEORIS <samuel.seda@neoris.com>
2024-06-25 10:34:30 -0600

    add func main to removeElement exercise

--- LeetCodeExcercices/TopInterview150/27.RemoveElement/removeElement.go
+++ LeetCodeExcercices/TopInterview150/27.RemoveElement/removeElement.go
@@ -1,13 +1,30 @@
-//isolated solution
-func removeElement(nums []int, val int) int {
+package main
+
+import "fmt"
+
+// isolated solution
+func removeElement(nums *[]int, val int) int {
 	founds := 0
-	numLen := len(nums)
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == val {
-			nums = append(nums[:i], nums[i+1:]...)
+	numLen := len(*nums)
+	for i := 0; i < len(*nums); i++ {
+		if (*nums)[i] == val {
+			(*nums) = append((*nums)[:i], (*nums)[i+1:]...)
 			founds++
 			i--
 		}
 	}
 	return numLen - founds
+}
+
+func main() {
+	nums := []int{0, 1, 2, 2, 3, 0, 4, 2}
+	remove := 2
+
+	fmt.Println("Nums", nums)
+	fmt.Println("Remove", remove)
+
+	result := removeElement(&nums, remove)
+
+	fmt.Println("Result", result)
+	fmt.Println("Nums", nums)
 }
